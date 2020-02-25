222547d354e5e3c21189153ac8105513c4c9403f
Jonathan East <jonathan_east@ymail.com>
2020-02-25 17:16:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ const playlist = {
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, {[artistName]: songTitle});
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
